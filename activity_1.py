1692631e33f3dc8e68b8be133cc4ec940a0af117
riszyshi <krista.mikah.agunod@aclcbutuan.edu.ph>
2024-06-19 12:35:53 +0800

    second version

--- activity_1.py
+++ activity_1.py
@@ -1,11 +1,15 @@
 word = "summer bootcamp"
+#1
 print(word.title())
+#2
 print(f"{word[0].upper()}{word[1:14]}{word[-1].upper()}")
-
+#3
 s = "L"
 print(f"{s}{word[8:11]}")
-
+#4
+print(f"{word[11:14]}{word[4].upper()}")
+#5
 print(f"{word[-3].upper()}{word[5].upper()}")
-
+#6
 new = word.replace(" ", "l")
 print (new.isalpha())
